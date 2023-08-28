8cc8123dae2595a751efb8afe5e706e0ed748c16
Hammad <hmdshfq@outlook.com>
2023-08-28 20:44:30 +0200

    fix: add logic function with array of cell ids

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let userTurn = true;
 
+function logic() {
+    // Array of id names of cells
+    let cellIds = [
+        'cell-1',
+        'cell-2',
+        'cell-3',
+        'cell-4',
+        'cell-5',
+        'cell-6',
+        'cell-7',
+        'cell-8',
+        'cell-9',
+    ];
+
+}
 
 function mark(event) {
     let input = event.target;
