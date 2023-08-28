52c22f4c7d5f81d3a6a7101d691fd2a17b2f2a9c
Hammad <hmdshfq@outlook.com>
2023-08-28 20:43:19 +0200

    fix: add mark function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let userTurn = true;
 
+
+function mark(event) {
+    let input = event.target;
+
+    if (userTurn === true) {
+        input.value = 'X';
+        input.disabled = true;
+        userTurn = false;
+    } else {
+        input.value = 'O';
+        input.disabled = true;
+        userTurn = true;
+    }
+}
+
 function reset() {
     window.location.reload();
 }
