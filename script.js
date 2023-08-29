51fc6eb3617701c93e66108494bed30d5aca4c77
Hammad <hmdshfq@outlook.com>
2023-08-29 19:02:02 +0200

    fix: add score board logic

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let userTurn = true;
 let nextPlayer = document.querySelector('#next-player');
+let winsPlayerX = localStorage.getItem('X');
+let winsPlayerO = localStorage.getItem('O');
+let playerXScore = document.querySelector('#player-x-score');
+let playerOScore = document.querySelector('#player-o-score');
+
+(function checkPlayerWins() {
+    if (!winsPlayerX && !winsPlayerO) {
+        localStorage.setItem('X', 0);
+        localStorage.setItem('O', 0);
+        window.location.reload();
+    }
+    playerXScore.textContent = winsPlayerX;
+    playerOScore.textContent = winsPlayerO;
+})();
 
 function playGame(event) {
     if (event.code === 'Space' || event.type === 'click') {
@@ -7,7 +21,6 @@ function playGame(event) {
         logic();
     }
 }
-
 
 function logic() {
     // Array of id names of cells
@@ -29,9 +42,7 @@ function logic() {
     );
 
     // Array of cell values
-    let cellValues = cellElements.map(
-        (element) => element.value
-    );
+    let cellValues = cellElements.map((element) => element.value);
 
     // Array of winning patterns
     let winPatterns = [
@@ -46,16 +57,18 @@ function logic() {
     ];
 
     // Number of empty cells left
-    let emptyCells = cellValues.filter(value => value === '').length;
+    let emptyCells = cellValues.filter((value) => value === '').length;
 
     // Evaluate if X or O wins or it's a draw
-    winPatterns.forEach(pattern => {
+    winPatterns.forEach((pattern) => {
         if (
             cellValues[pattern[0]] === 'X' &&
             cellValues[pattern[1]] === 'X' &&
             cellValues[pattern[2]] === 'X'
         ) {
             disableCells();
+            localStorage.setItem('X', Number(winsPlayerX) + 1);
+            playerXScore.textContent = localStorage.getItem('X');
             nextPlayer.textContent = 'Player X Wins!';
         } else if (
             cellValues[pattern[0]] === 'O' &&
@@ -63,6 +76,8 @@ function logic() {
             cellValues[pattern[2]] === 'O'
         ) {
             disableCells();
+            localStorage.setItem('O', Number(winsPlayerO) + 1);
+            playerOScore.textContent = localStorage.getItem('O');
             nextPlayer.textContent = 'Player O Wins!';
         } else if (emptyCells === 0) {
             nextPlayer.textContent = 'Match Tied!';
@@ -70,7 +85,7 @@ function logic() {
     });
 
     function disableCells() {
-        cellElements.forEach(cell => cell.disabled = true);
+        cellElements.forEach((cell) => (cell.disabled = true));
     }
 }
 
@@ -80,16 +95,28 @@ function mark(event) {
     if (userTurn === true) {
         input.value = 'X';
         input.disabled = true;
-        nextPlayer.textContent = 'Player O\'s Turn'
+        nextPlayer.textContent = "Player O's Turn";
         userTurn = false;
     } else {
         input.value = 'O';
         input.disabled = true;
-        nextPlayer.textContent = 'Player X\'s Turn'
+        nextPlayer.textContent = "Player X's Turn";
         userTurn = true;
     }
 }
 
-function reset() {
+function restart() {
     window.location.reload();
+}
+
+function reset() {
+    let message = 'Do you really want to RESET the score?';
+    if (confirm(message) == true) {
+        localStorage.clear();
+        playerXScore.textContent = 0;
+        playerOScore.textContent = 0;
+        alert('Score Reset!');
+    } else {
+        alert('Score kept!');
+    }
 }
